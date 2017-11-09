eae9e256dcfabcc9a1bcf56bfce732dbfabe6909
bjooris <Bart.Jooris@UGent.be>
2017-11-09 12:28:05 +0100

    wait for server to come available 5minutes

--- AutomatedTesting/wilabt/wavesRSSI/run.sh
+++ AutomatedTesting/wilabt/wavesRSSI/run.sh
@@ -14,6 +14,7 @@ EXP=`echo $HOSTNAME | cut -d '.' -f 2`
 NODE=`echo $HOSTNAME | cut -d '.' -f 1`
 NODEEXT=`echo $HOSTNAME | cut -d '.' -f 2-`
 RSSISERVERHOST=`echo node0.$NODEEXT`
+RSSISERVERPORT=57404
 
 CONTROLINT=ERROR
 CONTROLIP=ERROR
@@ -115,13 +116,27 @@ DEVNAME=`motelist -c | cut -d ',' -f 1`
 
 
 if [ "$NODE" == "node0" ]; then
-	prepareServer "RSSIserver" 56704
+	prepareServer "RSSIserver" $RSSISERVERPORT
+else
+	#!/bin/bash
+	SERVERAVAILABLE=0
+	RETRIES=60
+	while [ "$SERVERAVAILABLE" -eq "0" -a "$RETRIES" -gt "0" ]; do
+        if [ ! "" = "`sudo /usr/bin/nmap -PN -n $RSSISERVERHOST -sU -p $RSSISERVERPORT -g $RSSISERVERPORT 2>/dev/null | grep 'udp open' 2>/dev/null`" ]; then
+                SERVERAVAILABLE=1
+        else
+                sleep 5
+                RETRIES=$((RETRIES - 1))
+                echo Will wait for server $RSSISERVERHOST for another $((RETRIES*5)) seconds
+         fi
+	done
 fi
+
 
 echo "fping -aq -r 1 -i 50 $RSSISERVERHOST 2>/dev/null"
 
 if [ ! -z `fping -aq -r 1 -i 50 $RSSISERVERHOST 2>/dev/null` ]; then
-		preparePipeAndSocket "RSSIserver" 56704 PIPE_RSSISERVER
+		preparePipeAndSocket "RSSIserver" $RSSISERVERPORT PIPE_RSSISERVER
 fi
 
 sg dialout "python cc2538-bsl.py -e -w -v -a 0x00202000 -p $DEV -i ab:cd:00:ff:fe:00:00:1 $WSNBIN 2>&1"
