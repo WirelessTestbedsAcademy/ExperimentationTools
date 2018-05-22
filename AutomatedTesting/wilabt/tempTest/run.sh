a0f9a323ef59550d317d82f3402279dbae7c0cfb
pbecue <pbecue@users.noreply.github.com>
2018-05-22 16:38:05 +0200

    Update run.sh

--- AutomatedTesting/wilabt/tempTest/run.sh
+++ AutomatedTesting/wilabt/tempTest/run.sh
@@ -74,6 +74,7 @@ chmod a+x serialdump-linux
 
 sg dialout "python cc2538-bsl.py -e -w -v -a 0x00202000 -p $DEV -i ab:cd:00:ff:fe:00:00:1 tempTest.bin 2>&1"
 sg dialout "timeout 3 ./serialdump-linux -b115200 $DEV 2>&1"
+sleep 5
 TEMP=`sg dialout "timeout 3 ./serialdump-linux -b115200 $DEV | grep Temp | tail -n 1 | cut -f 3 -d ' '"`
 echo "TEMP = $TEMP"
 
